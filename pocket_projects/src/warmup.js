79209f8e9ca464e457e4d35cbc0c415830c02291
Yan Rivera <yan.rivera.6@gmail.com>
2022-10-04 16:27:59 -0700

    Render time on screen

--- pocket_projects/src/warmup.js
+++ pocket_projects/src/warmup.js
@@ -2,7 +2,9 @@
 const partyHeader = document.getElementById('party');
 
 export const htmlGenerator = (string, htmlElement) => {
-
+	const newTag = document.createElement("p");
+	newTag.innerHTML = string;
+	htmlElement.appendChild(newTag);
 };
 
 htmlGenerator('Party Time.', partyHeader);
